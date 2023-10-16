94e77f8c80c20ed7086f4de6b35c23e0b4c6d5f2
YSY <shaoyuanyu@foxmail.com>
2023-10-16 23:34:51 +0800

    fix:修正主界面参数

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import Editor from '@/components/code_editor/CodeEditor.vue'
+// models
+import type { Appearance } from "@/components/code_editor/models/appearance"
 
 const currentLang = 'c'
-
+const appearance: Appearance = {
+  isDark: true,
+  fontSize: 16
+}
 </script>
 
 <template>
   <div style="width: 80%; height: 100vh; padding: 20px 20px; background-color: antiquewhite">
-    <Editor :language="currentLang" :is-dark="false" />
+    <Editor :language="currentLang" :appearance="appearance" />
   </div>
 </template>
 
