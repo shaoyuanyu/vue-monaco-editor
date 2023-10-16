7b310525fd8912133ecb7157dc88fab139efc60c
YSY <shaoyuanyu@foxmail.com>
2023-10-16 23:31:35 +0800

    feat:增加接口参数

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 import Editor from '@/components/code_editor/CodeEditor.vue'
 
 const currentLang = 'c'
+
 </script>
 
 <template>
   <div style="width: 80%; height: 100vh; padding: 20px 20px; background-color: antiquewhite">
-    <Editor :language="currentLang" />
+    <Editor :language="currentLang" :is-dark="false" />
   </div>
 </template>
 
